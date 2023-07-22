9bd415cbd97f1d120f9af6dabf8d35f539f68699
DeepakJavaDev <dzunzunkar@gmail.com>
2023-07-22 10:22:53 +0530

    project setup and basics of thymeleaf

---
+++ src/main/java/com/dz/app/SpringBootThymeleafDemo3Application.java
@@ -0,0 +1,13 @@
+package com.dz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafDemo3Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/dz/app/controller/HomeController.java
@@ -0,0 +1,38 @@
+package com.dz.app.controller;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class HomeController {
+
+	
+	//http://localhost:8081/home
+	@GetMapping("home")
+	public String home(Model model) {
+		System.out.println("Inside home page handler..");
+		
+		model.addAttribute("topic1","Welcome to home page...!");
+		model.addAttribute("currentdate",new Date().toLocaleString());
+		
+		model.addAttribute("friends",Arrays.asList("kundan","sachin","manish","ganu"));
+		
+		model.addAttribute("isActive",false);
+		model.addAttribute("gender","F");
+		
+		model.addAttribute("mlist",Arrays.asList(10));
+		
+		model.addAttribute("brief","Just Chill & Relax and Do what you want in your life...");
+		model.addAttribute("mobile","(+91) 99999999");
+		model.addAttribute("email","testme@gmail.com");
+		
+		return "home";
+		//view name --> home.html
+	}
+}
